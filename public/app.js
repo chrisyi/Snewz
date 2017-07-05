19f62e029e44908f7a7f87d4f944e859e6f5f6b3
cy <cy@Christophers-MacBook-Pro.local>
2017-07-05 09:41:22 -0500

    yesterday work

--- public/app.js
+++ public/app.js
@@ -1,1 +1,11 @@
-angular.module('app', [])   
+angular.module('app', ['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: './views/home/home.html',
+                controller: 'homeCtrl'
+            })
+        $urlRouterProvider
+            .otherwise('/')
+    })
---
+++ public/views/home/homeCtrl.js
@@ -0,0 +1,10 @@
+angular.module('app')
+    .controller('homeCtrl', function ($scope, homeService) {
+
+        $scope.test = 'home page success'
+
+
+
+
+
+    })
