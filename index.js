c8eddf6eef197883b528227f2fc6a45bb161bb06
cy <cy@Christophers-MacBook-Pro.local>
2017-07-14 15:53:07 -0500

    added database

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
+require('dotenv').config();
+
 const express = require('express'),
     cors = require('cors'),
     bodyParser = require('body-parser'),
     app = module.exports = express(),
     massive = require('massive'),
-    session = require('express-session'),
-    connectionString = 'postgres://cy@localhost/articlesDB';
+    session = require('express-session')
+// connectionString = 'postgres://cy@localhost/articlesDB';
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/", express.static(__dirname + '/public'));
-massive(connectionString).then(dbInstance => {
+massive(process.env.DATABASE_URL).then(dbInstance => {
     app.set('db', dbInstance)
 })
-
-
 
 app.post('/savedArticles', function (req, res, next) {
     console.log(req.body);
@@ -39,8 +39,7 @@ app.delete('/deleteArticle/:id', function (req, res, next) {
         .catch(err => res.status(404).json(response))
 })
 
-
-const port = 3000;
+// const port = 3000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}.`);
 });
